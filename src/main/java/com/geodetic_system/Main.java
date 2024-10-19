ec254ab0dfc5e30f4548ecf2a48be0948b73ba1b
timea.funtik <timeafuntik@gmail.com>
2024-10-19 23:39:29 +0200

    added my own few examples to check desired behaviour of the structure

--- src/main/java/com/geodetic_system/Main.java
+++ src/main/java/com/geodetic_system/Main.java
@@ -1,7 +1,5 @@
 package com.geodetic_system;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,52 +9,50 @@ public class Main {
     private static final Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        setFormattingOnConsole(log);
+        setFormattingOnConsole();
 
         Main mainInstance = new Main();
         mainInstance.runParcelaExample();
         mainInstance.runPropertyExample();
     }
 
-    private static void setFormattingOnConsole(Logger log) {
+    private static void setFormattingOnConsole() {
         ConsoleHandler consoleHandler = new ConsoleHandler();
         consoleHandler.setLevel(Level.ALL);
         consoleHandler.setFormatter(new SimpleFormatter());
-        log.addHandler(consoleHandler);
-        log.setUseParentHandlers(false);
-        log.setLevel(Level.FINE);
+        Main.log.addHandler(consoleHandler);
+        Main.log.setUseParentHandlers(false);
+        Main.log.setLevel(Level.FINE);
     }
 
     // Parcely
+
+
     private void runParcelaExample() {
         final int POCET_DIMENZII = 2;
+        GPSPosition gpsPosition1 = new GPSPosition('N', 48.02, 'E', 17.02);
+        GPSPosition gpsPosition2 = new GPSPosition('N', 48.05, 'E', 17.05);
+        GPSPosition gpsPosition3 = new GPSPosition('N', 48.10, 'E', 17.10);
+        GPSPosition gpsPosition4 = new GPSPosition('N', 48.07, 'E', 17.07);
+        GPSPosition gpsPosition5 = new GPSPosition('N', 48.15, 'E', 17.09);
+        GPSPosition gpsPosition6 = new GPSPosition('N', 48.09, 'E', 17.12);
 
-        GPSPosition position1 = new GPSPosition('N', 48.150, 'E', 17.100);
-        GPSPosition position2 = new GPSPosition('N', 48.100, 'E', 17.050);
-        GPSPosition position3 = new GPSPosition('N', 48.200, 'E', 17.200);
-        GPSPosition position4 = new GPSPosition('N', 48.050, 'E', 17.020);
-        GPSPosition position5 = new GPSPosition('N', 48.120, 'E', 17.080);
-        GPSPosition position6 = new GPSPosition('N', 48.180, 'E', 17.180);
-        GPSPosition position7 = new GPSPosition('N', 48.250, 'E', 17.300);
+        Parcela parcela1 = new Parcela(1, "Parcela 1", gpsPosition1);
+        Parcela parcela2 = new Parcela(2, "Parcela 2", gpsPosition2);
+        Parcela parcela3 = new Parcela(3, "Parcela 3", gpsPosition3);
+        Parcela parcela4 = new Parcela(4, "Parcela 4", gpsPosition4);
+        Parcela parcela5 = new Parcela(5, "Parcela 5", gpsPosition5);
+        Parcela parcela6 = new Parcela(6, "Parcela 6", gpsPosition6);
 
-        Parcela parcela1 = new Parcela(1, "Parcela 1", position1, position1);
-        Parcela parcela2 = new Parcela(2, "Parcela 2", position2, position2);
-        Parcela parcela3 = new Parcela(3, "Parcela 3", position3, position3);
-        Parcela parcela4 = new Parcela(4, "Parcela 4", position4, position4);
-        Parcela parcela5 = new Parcela(5, "Parcela 5", position5, position5);
-        Parcela parcela6 = new Parcela(6, "Parcela 6", position6, position6);
-        Parcela parcela7 = new Parcela(7, "Parcela 7", position7, position7);
-
-        KDTree<Parcela, Property> kdTreeParcela = new KDTree<>();
+        log.info("Parcela example-------------------------------------");
+        KDTree<Parcela> kdTreeParcela = new KDTree<>();
         kdTreeParcela.insert(parcela1, POCET_DIMENZII);
         kdTreeParcela.insert(parcela2, POCET_DIMENZII);
         kdTreeParcela.insert(parcela3, POCET_DIMENZII);
         kdTreeParcela.insert(parcela4, POCET_DIMENZII);
         kdTreeParcela.insert(parcela5, POCET_DIMENZII);
         kdTreeParcela.insert(parcela6, POCET_DIMENZII);
-        kdTreeParcela.insert(parcela7, POCET_DIMENZII);
 
-        //printTree(kdTreeParcela.getRoot());
     }
 
     private void runPropertyExample() {
@@ -70,15 +66,16 @@ public class Main {
         GPSPosition position6 = new GPSPosition('N', 48.181, 'E', 17.181);
         GPSPosition position7 = new GPSPosition('N', 48.271, 'E', 17.311);
 
-        Property property1 = new Property(100, "Property 1", position1, position1);
-        Property property2 = new Property(101, "Property 2", position2, position2);
-        Property property3 = new Property(102, "Property 3", position3, position3);
-        Property property4 = new Property(103, "Property 4", position4, position4);
-        Property property5 = new Property(104, "Property 5", position5, position5);
-        Property property6 = new Property(105, "Property 6", position6, position6);
-        Property property7 = new Property(106, "Property 7", position7, position7);
+        Property property1 = new Property(100, "Property 1", position1);
+        Property property2 = new Property(101, "Property 2", position2);
+        Property property3 = new Property(102, "Property 3", position3);
+        Property property4 = new Property(103, "Property 4", position4);
+        Property property5 = new Property(104, "Property 5", position5);
+        Property property6 = new Property(105, "Property 6", position6);
+        Property property7 = new Property(106, "Property 7", position7);
 
-        KDTree<Property, Parcela> kdTreeProperty = new KDTree<>();
+        log.info("Property example-------------------------------------");
+        KDTree<Property> kdTreeProperty = new KDTree<>();
         kdTreeProperty.insert(property1, POCET_DIMENZII);
         kdTreeProperty.insert(property2, POCET_DIMENZII);
         kdTreeProperty.insert(property3, POCET_DIMENZII);
@@ -86,30 +83,6 @@ public class Main {
         kdTreeProperty.insert(property5, POCET_DIMENZII);
         kdTreeProperty.insert(property6, POCET_DIMENZII);
         kdTreeProperty.insert(property7, POCET_DIMENZII);
-
-        //printTree(kdTreeProperty.getRoot());
     }
 
-    private <T extends IObjectInSystem<T, R>, R> void printTree(KDNode<T, R> root) {
-        List<List<String>> levels = new ArrayList<>();
-        buildTreeLevels(root, 0, levels);
-        printCenteredTree(levels);
-    }
-
-    private <T extends IObjectInSystem<T, R>, R> void buildTreeLevels(KDNode<T, ?> node, int level, List<List<String>> levels) {
-        if (node == null) return;
-        if (levels.size() <= level) levels.add(new ArrayList<>());
-        levels.get(level).add("(" + node.getData().getTopLeft().getLatitude() + ", " + node.getData().getTopLeft().getLongitude() + ")");
-        buildTreeLevels(node.getLeft(), level + 1, levels);
-        buildTreeLevels(node.getRight(), level + 1, levels);
-    }
-
-    private void printCenteredTree(List<List<String>> levels) {
-        int maxLevel = levels.size();
-        for (int i = 0; i < levels.size(); i++) {
-            int levelWidth = (int) Math.pow(2, maxLevel - i - 1);
-            String line = String.join(" ".repeat(levelWidth), levels.get(i));
-            System.out.println(" ".repeat(levelWidth) + line);
-        }
-    }
 }
