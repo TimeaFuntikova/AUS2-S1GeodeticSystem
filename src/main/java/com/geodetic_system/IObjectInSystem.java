da63cb8fed17f10b99ab404e139268de75cc4a6d
timea.funtik <timeafuntik@gmail.com>
2024-10-19 23:37:51 +0200

    refactored Interface- removed reference lists generics

--- src/main/java/com/geodetic_system/IObjectInSystem.java
+++ src/main/java/com/geodetic_system/IObjectInSystem.java
@@ -1,14 +1,8 @@
 package com.geodetic_system;
 
-import java.util.List;
-
-public interface IObjectInSystem <T extends IObjectInSystem<T,R>, R> {
+public interface IObjectInSystem<T> {
     int getId(); // supuisne cislo pre property alebo parcelu
     String getDescription();
-    GPSPosition getTopLeft();
-    GPSPosition getBottomRight();
-    List<R> getRelatedObjects(); // vrati zoznam pripojenych objektov
-    void addRelatedObject(R obj); // prida objekt do zoznamu pripojenych objektov
-    //pouižitie T = metody budu bezpecne voci typovym chybam
-    int compareByID(T other);
+    GPSPosition getGpsPosition();
+    int compareByDimension(T other, int POCET_DIMENZII, int depth);
 }
--- src/main/java/com/geodetic_system/KDNode.java
+++ src/main/java/com/geodetic_system/KDNode.java
@@ -1,9 +1,11 @@
 package com.geodetic_system;
 
-public class KDNode<T extends IObjectInSystem<T, R>, R> {
+public class KDNode<T extends IObjectInSystem<T>> {
 
     private final T data;
-    private KDNode<T, R> left, right, parent;
+    private KDNode<T> left;
+    private KDNode<T> right;
+    private KDNode<T> parent;
 
     public KDNode(T data) {
         this.data = data;
@@ -16,27 +18,27 @@ public class KDNode<T extends IObjectInSystem<T, R>, R> {
         return data;
     }
 
-    public KDNode<T, R> getLeft() {
+    public KDNode<T> getLeft() {
         return left;
     }
 
-    public void setLeft(KDNode<T, R> left) {
+    public void setLeft(KDNode<T> left) {
         this.left = left;
     }
 
-    public KDNode<T, R> getRight() {
+    public KDNode<T> getRight() {
         return right;
     }
 
-    public void setRight(KDNode<T, R> right) {
+    public void setRight(KDNode<T> right) {
         this.right = right;
     }
 
-    public KDNode<T, R> getParent() {
+    public KDNode<T> getParent() {
         return parent;
     }
 
-    public void setParent(KDNode<T, R> parent) {
+    public void setParent(KDNode<T> parent) {
         this.parent = parent;
     }
 }
