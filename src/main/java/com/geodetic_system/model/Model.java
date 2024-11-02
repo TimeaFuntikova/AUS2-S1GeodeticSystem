1b56e01019756fedbfb3657a923300b3fbdd6fb7
timea.funtik <timeafuntik@gmail.com>
2024-11-02 15:22:18 +0100

    Model nicely

--- src/main/java/com/geodetic_system/model/Model.java
+++ src/main/java/com/geodetic_system/model/Model.java
@@ -15,15 +15,18 @@ import com.geodetic_system.visual.GUIObject;
  */
 public class Model  {
 
-
     private final KDTree<GeodeticObject> kdTree = new KDTree<>();
     private final int POCET_DIMENZII = 2;
 
     public Model() {
-    //emtpy constructor
+        //empty
     }
 
-
+    /**
+     * Creates a GeodeticObject from a GUIObject
+     * @param guiObject the object to create
+     * @return the created GeodeticObject
+     */
     public GeodeticObject createGeodeticObject(GUIObject guiObject) {
         return switch (guiObject.getObjectToAssign()) {
             case "PARCELA" -> createParcela(guiObject);
@@ -32,30 +35,65 @@ public class Model  {
         };
     }
 
+    /**
+     * Inserts a GeodeticObject into the database
+     * @param geodeticObject the object to insert
+     * @return true if the object was inserted, false otherwise
+     */
     public boolean insertGeodeticObject(GeodeticObject geodeticObject) {
         return this.kdTree.insert(geodeticObject, POCET_DIMENZII);
     }
 
+    /**
+     * Finds a GeodeticObject in the database
+     * @param geodeticObject the object to find
+     * @return the found GeodeticObject
+     */
     public GeodeticObject findGeodeticObject(GeodeticObject geodeticObject) {
         return this.kdTree.find(geodeticObject, POCET_DIMENZII);
     }
 
+    /**
+     * Deletes a GeodeticObject from the database
+     * @param geodeticObject the object to delete
+     * @return true if the object was deleted, false otherwise
+     */
     public boolean deleteGeodeticObject(GeodeticObject geodeticObject) {
         return this.kdTree.delete(geodeticObject, POCET_DIMENZII);
     }
 
+    /**
+     * Creates a Parcela from a GUIObject
+     * @param guiObject the object to create
+     * @return the created Parcela object
+     */
     private GeodeticObject createParcela(GUIObject guiObject) {
         return new Parcela(guiObject.getId(), guiObject.getDescription(), createTopLeftGPSPosition(guiObject), createBottomRightGPSPosition(guiObject));
     }
 
+    /**
+     * Creates a Property from a GUIObject
+     * @param guiObject the object to create
+     * @return the created Property object
+     */
     private GeodeticObject createProperty(GUIObject guiObject) {
         return new Property(guiObject.getId(), guiObject.getDescription(), createTopLeftGPSPosition(guiObject), createBottomRightGPSPosition(guiObject));
     }
 
+    /**
+     * Creates a top left GPSPosition from a GUIObject
+     * @param guiObject the object to create
+     * @return the created GPSPosition object
+     */
     private GPSPosition createTopLeftGPSPosition(GUIObject guiObject) {
         return new GPSPosition('N', guiObject.getGPSPositionTopLeftX(), 'E', guiObject.getGPSPositionTopLeftY());
     }
 
+    /**
+     * Creates a bottom right GPSPosition from a GUIObject
+     * @param guiObject the object to create
+     * @return the created GPSPosition object
+     */
     private GPSPosition createBottomRightGPSPosition(GUIObject guiObject) {
         return new GPSPosition('N', guiObject.getGPSPositionBottomRightX(), 'E', guiObject.getGPSPositionBottomRightY());
     }
